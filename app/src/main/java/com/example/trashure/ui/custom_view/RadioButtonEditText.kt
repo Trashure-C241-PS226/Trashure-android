c37e66f6a4e0234258cc31124924f16c7eae0f3a
rafputraa <rafifadil0603@gmail.com>
2024-06-17 06:21:32 +0700

    Slicing collect page
    radiobutton perlu dipanggil fungsi setOptions di activity
    
    font hint inter regular gaada, jadinya pake manrope
    
    duration of use berapa lama satuannya years/month

---
+++ app/src/main/java/com/example/trashure/ui/custom_view/RadioButtonEditText.kt
@@ -0,0 +1,52 @@
+package com.example.trashure.ui.custom_view
+
+import android.app.AlertDialog
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.appcompat.widget.AppCompatEditText
+
+class RadioButtonEditText : AppCompatEditText, View.OnClickListener {
+
+    private var options: Array<String> = arrayOf("Apple", "Samsung", "Google")
+
+    constructor(context: Context) : super(context) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init()
+    }
+
+    constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init()
+    }
+
+    private fun init() {
+        isFocusable = false
+        isClickable = true
+        setOnClickListener(this)
+    }
+
+    fun setOptions(options: Array<String>) {    // panggil ini di activity
+        this.options = options
+    }
+
+    override fun onClick(v: View?) {
+        if (options.isNotEmpty()) {
+            val builder = AlertDialog.Builder(context)
+            builder.setTitle("Choose an option")
+
+            builder.setSingleChoiceItems(options, -1) { dialog, which ->
+                setText(options[which])
+                dialog.dismiss()
+            }
+
+            builder.show()
+        }
+    }
+}
